5803d3cf987290ea9af12647b350eccd38c629fc
Razzberry7 <68514613+Razzberry7@users.noreply.github.com>
2023-10-13 10:52:42 -0400

    Additional Changes

--- CVAT-Export-Helper.py
+++ CVAT-Export-Helper.py
@@ -10,11 +10,12 @@
 
 # USAGE: python ./CVAT-Export-Helper.py
 
+import sys
 import os
 import shutil
-from shutil import make_archive
 from tkinter import *
 from tkinter import filedialog
+from tkinter.ttk import Progressbar
 from zipfile import ZipFile
 from paramiko import SSHClient
 from scp import SCPClient
@@ -42,103 +43,102 @@ def zip_folder(path, zipf):
 
 # Function creating aforementioned structure
 def export_helper(path):
-    if path != "":
-        path_origin = os.path.dirname(path) + "/"
-        filename = os.path.basename(path[:-4])
+    try:
+        if path != "":
+            path_origin = os.path.dirname(path) + "/"
+            filename = os.path.basename(path[:-4])
 
-        print(path_origin)
-        print(filename)
+            print(path_origin)
+            print(filename)
 
-        # Making first folder if it doesn't exist
-        dir_path = path_origin + filename
-        if os.path.exists(dir_path):
-            shutil.rmtree(dir_path)
-            os.mkdir(dir_path)
-        else:
-            os.mkdir(dir_path)
+            # Making first folder if it doesn't exist
+            dir_path = path_origin + filename
+            if os.path.exists(dir_path):
+                shutil.rmtree(dir_path)
+                os.mkdir(dir_path)
+            else:
+                os.mkdir(dir_path)
 
-        # Making dataset folder if it doesn't exist
-        dir_path = path_origin + filename + "/" + folder_settings_var.get()
-        if os.path.exists(dir_path):
-            shutil.rmtree(dir_path)
-            os.mkdir(dir_path)
-        else:
-            os.mkdir(dir_path)
+            # Making dataset folder if it doesn't exist
+            dir_path = path_origin + filename + "/" + folder_settings_var.get()
+            if os.path.exists(dir_path):
+                shutil.rmtree(dir_path)
+                os.mkdir(dir_path)
+            else:
+                os.mkdir(dir_path)
 
-        # Making images folder if it doesn't exist
-        images_path = path_origin + filename + "/" + folder_settings_var.get() + "/images"
-        if os.path.exists(images_path):
-            shutil.rmtree(images_path)
-            os.mkdir(images_path)
-        else:
-            os.mkdir(images_path)
+            # Making images folder if it doesn't exist
+            images_path = path_origin + filename + "/" + folder_settings_var.get() + "/images"
+            if os.path.exists(images_path):
+                shutil.rmtree(images_path)
+                os.mkdir(images_path)
+            else:
+                os.mkdir(images_path)
 
-        # Making labels folder if it doesn't exist
-        labels_path = path_origin + filename + "/" + folder_settings_var.get() + "/labels"
-        if os.path.exists(labels_path):
-            shutil.rmtree(labels_path)
-            os.mkdir(labels_path)
-        else:
-            os.mkdir(labels_path)
+            # Making labels folder if it doesn't exist
+            labels_path = path_origin + filename + "/" + folder_settings_var.get() + "/labels"
+            if os.path.exists(labels_path):
+                shutil.rmtree(labels_path)
+                os.mkdir(labels_path)
+            else:
+                os.mkdir(labels_path)
 
-        # Create a data.yaml file
-        dir_path = path_origin + filename + "/"
-        with open(dir_path + '/data.yaml', 'w') as f:
-            f.write('train: ./data/training_data/' + filename + '/train/images\n')
-            f.write('val: ./data/training_data/' + filename + '/valid/images\n')
-            f.write('test: ./data/training_data/' + filename + '/test/images\n')
-            f.write('\n')
-            if class_settings_var.get() == "Berry":
-                f.write('nc: 2\n')
-                f.write("names: ['blue', 'green']")
-            if class_settings_var.get() == "Bush":
-                f.write('nc: 1\n')
-                f.write("names: ['bush']")
-            if class_settings_var.get() == "Scorch":
-                f.write('nc: 1\n')
-                f.write("names: ['scorch']")
+            # Create a data.yaml file
+            dir_path = path_origin + filename + "/"
+            with open(dir_path + '/data.yaml', 'w') as f:
+                f.write('train: ./data/training_data/' + filename + '/train/images\n')
+                f.write('val: ./data/training_data/' + filename + '/valid/images\n')
+                f.write('test: ./data/training_data/' + filename + '/test/images\n')
+                f.write('\n')
+                if class_settings_var.get() == "Berry":
+                    f.write('nc: 2\n')
+                    f.write("names: ['blue', 'green']")
+                if class_settings_var.get() == "Bush":
+                    f.write('nc: 1\n')
+                    f.write("names: ['bush']")
+                if class_settings_var.get() == "Scorch":
+                    f.write('nc: 1\n')
+                    f.write("names: ['scorch']")
 
-        with ZipFile(path, 'r') as zip:
-            for file in zip.namelist():
-                if 'obj_train_data' in file and (file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg")):
-                    zip.extract(file, images_path)
-                if 'obj_train_data' in file and file.endswith(".txt"):
-                    zip.extract(file, labels_path)
+            with ZipFile(path, 'r') as zip:
+                for file in zip.namelist():
+                    if 'obj_train_data' in file and (file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg")):
+                        zip.extract(file, images_path)
+                    if 'obj_train_data' in file and file.endswith(".txt"):
+                        zip.extract(file, labels_path)
 
-        for file in os.listdir(images_path + "/obj_train_data"):
-            shutil.move(os.path.join(images_path + "/obj_train_data", file), images_path)
-        shutil.rmtree(images_path + "/obj_train_data")
+            for file in os.listdir(images_path + "/obj_train_data"):
+                shutil.move(os.path.join(images_path + "/obj_train_data", file), images_path)
+            shutil.rmtree(images_path + "/obj_train_data")
 
-        for file in os.listdir(labels_path + "/obj_train_data"):
-            shutil.move(os.path.join(labels_path + "/obj_train_data", file), labels_path)
-        shutil.rmtree(labels_path + "/obj_train_data")
+            for file in os.listdir(labels_path + "/obj_train_data"):
+                shutil.move(os.path.join(labels_path + "/obj_train_data", file), labels_path)
+            shutil.rmtree(labels_path + "/obj_train_data")
 
-        zf = ZipFile(path_origin + filename + ".zip", "w")
-        zip_folder(path_origin + filename, zf)
-        zf.close()
+            zf = ZipFile(path_origin + filename + ".zip", "w")
+            zip_folder(path_origin + filename, zf)
+            zf.close()
+    except:
+        print("ERROR: Something went wrong when trying to convert the CVAT file. "
+              "\nAre you sure you choose the correct zip file?")
 
-        # zf = ZipFile(path_origin + filename + ".zip", "w")
-        # for dirname, subdirs, files in os.walk(path_origin + filename):
-        #     # zf.write(dirname)
-        #     for filename in files:
-        #         zf.write(os.path.join(dirname, filename),
-        #                  os.path.relpath(os.path.join(dirname, filename),
-        #                                  os.path.join(path_origin + filename, '..')))
-        # zf.close()
 
 def SCP_to_Lambda(file_path, export_path, username, password):
     # print(file_path)
     # print(export_path)
     # print(username)
     # print(password)
+    try:
+        if file_path and export_path and username and password != "":
+            ssh = SSHClient()
+            ssh.load_system_host_keys()
+            ssh.connect(hostname='lambda04.rowan.edu', port=22, username=username, password=password)
 
-    if file_path and export_path and username and password != "":
-        ssh = SSHClient()
-        ssh.load_system_host_keys()
-        ssh.connect(hostname='lambda04.rowan.edu', port=22, username=username, password=password)
-
-        with SCPClient(ssh.get_transport()) as scp:
-            scp.put(file_path, export_path)
+            with SCPClient(ssh.get_transport()) as scp:
+                scp.put(file_path, export_path)
+    except:
+        print("Something went wrong when trying to SCP onto Lambda."
+              "\nAre you sure you're connected to the Rowan VPN?")
 
 
 file_path = ""
@@ -152,7 +152,7 @@ root.title('CVAT Export Helper, by: Brandon McHenry')
 # Set window size
 root.geometry("850x300")
 
-root.iconbitmap("./berry.ico")
+# root.iconbitmap("./berry.ico")
 
 # Set window background color
 berry_blue = '#%02x%02x%02x' % (164, 206, 252)
@@ -200,7 +200,7 @@ button_scp = Button(buttons_frame,
 button_exit = Button(buttons_frame,
                      text="Close",
                      fg=text_color,
-                     command=exit)
+                     command=lambda: sys.exit())
 
 button_explore.pack(side=LEFT, padx=5, pady=5, ipadx=5, ipady=5)
 
@@ -332,20 +332,6 @@ scp_settings_password_tb = Entry(scp_settings_frame,
 scp_settings_password_tb.grid(row=2, column=1, padx=5, pady=5)
 
 
-# class_settings_bush_cb = Radiobutton(scp_settings_frame,
-#                                      text="Using bush classes",
-#                                      variable=scp_settings_var,
-#                                      fg=text_color, bg=outline_color,
-#                                      value="Bush")
-# class_settings_bush_cb.pack(side=LEFT)
-#
-# class_settings_scorch_cb = Radiobutton(scp_settings_frame,
-#                                        text="Using scorch classes",
-#                                        variable=scp_settings_var,
-#                                        fg=text_color, bg=outline_color,
-#                                        value="Scorch")
-# class_settings_scorch_cb.pack(side=LEFT)
-
 scp_settings_frame.pack(expand=True, fill='x') # Center the frame
 
 # </editor-fold>
@@ -354,7 +340,14 @@ settings_frame.grid(row=0, column=1)
 
 # </editor-fold>
 
-input_frame.pack(anchor=S, padx=10, pady=10,)
+# progress = Progressbar(root, orient=HORIZONTAL, length=500, mode='determinate')
+#
+# progress.pack(anchor=S)
+#
+# progress.start()
+
+
+input_frame.pack(anchor=S, padx=10, pady=10)
 
 # </editor-fold>
 
